3f29d3d9e77d69054ada88dd7d53ebf58c69da13
yaumu3 <yumu@me.com>
2020-05-30 16:24:00 +0900

    Initial release

---
+++ README.md
@@ -0,0 +1,102 @@
+# backlog-template
+
+Python CLI tool for posting template issues to Backlog project.
+
+![demo](demo.gif)
+
+## Requirements
+
+- Python 3.8 or later
+- Poetry 1.0.0 or later (dependency manager)
+
+## Installation
+
+Use [Poetry](https://python-poetry.org) to install dependent packages.
+
+```sh
+poetry install
+```
+
+## How to use
+
+After configured propery,
+
+1. Activate virtual environment
+2. Let the program eat template file written in [TOML](https://github.com/toml-lang/toml).
+
+```sh
+poetry shell
+python backlog_template.py templates/template.toml
+```
+
+## Configurations
+
+### Environment variables (required)
+
+Create `backlog_template.toml` to the same directory. All environment variables are stored in this TOML file.
+
+These values are required.
+
+- `SPACE_DOMAIN`: Backlog domain (ex: example.backlog.com)
+- `API_KEY`: Backlog API key (cf. [Backlog Help Center - API Settings](https://support.backlog.com/hc/en-us/articles/115015420567-API-Settings))
+- `PROJECT`: Backlog project name
+
+#### Example of `backlog_tempalte.toml`
+
+```toml
+[backlog_template]
+SPACE_DOMAIN = "your_space_name.backlog.com"
+API_KEY = "0123456789yOuRaPiKeY9876543210"
+PROJECT = "YOUR_PROJECT"
+```
+
+### Template
+
+Strings in curly braces will be replaced upon the post by the dictionary defined in `[repl]`.
+
+#### Mandatory keys
+
+- summary
+- issueType
+- priority
+
+#### Optional keys
+
+- description
+- milestone
+- version
+- dueDate (must be formatted as `yyyy-MM-dd`)
+- assignee
+
+#### Example of template
+
+```toml
+[repl]
+SUMMARY = "a issue"
+GOAL = "a goal"
+CHILD_DUE_DATE = "2025-01-01"
+
+[issue]
+summary = "{SUMMARY}"
+issueType = "Bug"
+description = "## TL;DR\n\nThis task must be done to achiveve {GOAL}."
+milestone = "1.0.0"
+priority = "High"
+assignee = "John Smith"
+
+[[issue.children]]
+summary = "a child issue of {SUMMARY}"
+issueType = "Task"
+description = "## a child issue of {SUMMARY}\nThis issue is essential to achieve {GOAL}."
+version = "1.0.0"
+milestone = "1.0.0"
+priority = "High"
+dueDate = "{CHILD_DUE_DATE}"
+assignee = "John Smith"
+
+[[issue.children]]
+summary = "another child issue of {SUMMARY}"
+issueType = "Task"
+description = "## another child issue of {SUMMARY}\nThis issue is really important to achieve {GOAL}."
+priority = "Low"
+```
---
+++ backlog_template.py
@@ -0,0 +1,173 @@
+from datetime import datetime
+from logging import INFO, basicConfig, getLogger
+from pathlib import Path
+
+import requests
+import toml
+
+logger = getLogger(__name__)
+
+
+class BaseAPI:
+    def get(self, end_point, params={}):
+        r = requests.get(end_point, params=params)
+        r.raise_for_status()
+        return r
+
+    def post(self, end_point, data, params={}):
+        r = requests.post(end_point, data, params=params)
+        r.raise_for_status()
+        return r
+
+
+class BacklogProject(BaseAPI):
+    """
+    Backlog API (V2)
+    https://developer.nulab.com/ja/docs/backlog/#backlog-api-とは
+    """
+
+    def __init__(self, api_key, space_domain, project):
+        def index(response, key="name", value="id"):
+            return {d[key]: d[value] for d in response.json()}
+
+        self.api_key = api_key
+        self.base_url = "https://" + space_domain + "/api/v2/"
+        self.project_id = index(self.get_prop("projects"))[project]
+        self.priorities = index(self.get_prop("priorities"))
+        self.pj_issue_types = index(self.get_pj_prop("issueTypes"))
+        self.pj_versions = index(self.get_pj_prop("versions"))
+        self.pj_users = index(self.get_pj_prop("users"))
+
+    @classmethod
+    def by_config(cls, path=None):
+        if path is None:
+            path = Path(__file__).resolve().parent.joinpath("backlog_template.toml")
+        config = toml.load(open(path))
+        b = config["backlog_template"]
+        return cls(b["API_KEY"], b["SPACE_DOMAIN"], b["PROJECT"])
+
+    def get_prop(self, property_name):
+        return self.get(
+            end_point=self.base_url + property_name, params={"apiKey": self.api_key},
+        )
+
+    def get_pj_prop(self, property_name):
+        return self.get(
+            end_point=self.base_url + f"projects/{self.project_id}/{property_name}",
+            params={"apiKey": self.api_key},
+        )
+
+    def post_issue(self, **kwargs):
+        return self.post(
+            end_point=self.base_url + "issues",
+            data={
+                "projectId": self.project_id,
+                "summary": kwargs["summary"],
+                "issueTypeId": kwargs["issueTypeId"],
+                "priorityId": kwargs["priorityId"],
+                "parentIssueId": kwargs.get("parentIssueId"),
+                "description": kwargs.get("description"),
+                "dueDate": kwargs.get("dueDate"),
+                "versionId[]": kwargs.get("versionId"),
+                "milestoneId[]": kwargs.get("milestoneId"),
+                "assigneeId": kwargs.get("assigneeId"),
+            },
+            params={"apiKey": self.api_key},
+        )
+
+    def post_issue_by_template(self, template):
+        return self.post_issue(
+            summary=template["summary"],
+            issueTypeId=self.pj_issue_types[template["issueType"]],
+            priorityId=self.priorities[template["priority"]],
+            parentIssueId=template.get("parentIssueId"),
+            description=template.get("description"),
+            dueDate=template.get("dueDate"),
+            versionId=self.pj_versions.get(template.get("version")),
+            milestoneId=self.pj_versions.get(template.get("milestone")),
+            assigneeId=self.pj_users.get(template.get("assignee")),
+        )
+
+    def post_affiliated_issues_by_template(self, template):
+        def replace(dic):
+            return {k: v.format(**repl) for k, v in dic.items()}
+
+        repl = template.pop("repl", {})
+        parent = template["issue"]
+        children = parent.pop("children", [])
+        # replace by repl(dict)
+        parent = replace(parent)
+        children = [replace(child) for child in children]
+        # template validation
+        self._validate_template(parent)
+        [self._validate_template(child) for child in children]
+
+        r = self.post_issue_by_template(parent).json()
+        logger.info(
+            "\nPosted an issue -> {} '{}'.".format(r["issueKey"], parent["summary"])
+        )
+        parentIssueId = r["id"]
+        for child in children:
+            # append parentIssueId to child then post
+            child.update({"parentIssueId": parentIssueId})
+            rc = self.post_issue_by_template(child).json()
+            logger.info(
+                "\nPosted a child issue of {} -> {} '{}'.".format(
+                    r["issueKey"], rc["issueKey"], child["summary"]
+                )
+            )
+
+    def _validate_template(self, template):
+        # validate if the template contains all mandatory keys
+        mandatory_keys = ("summary", "issueType", "priority")
+        for k in mandatory_keys:
+            assert k in template, "Template is missing a mandatory key '{}'".format(k)
+
+        # validate if KeyError does not occur
+        substitue_keys = {
+            "priority": "priorities",
+            "issueType": "pj_issue_types",
+            "version": "pj_versions",
+            "milestone": "pj_versions",
+            "assignee": "pj_users",
+        }
+        for k, v in substitue_keys.items():
+            if k not in template:
+                continue
+            assert template[k] in getattr(
+                self, v
+            ), "'{}' is not a valid key for '{}'".format(template[k], k)
+
+        # validate date format
+        date_keys = ("dueDate",)
+        for k in date_keys:
+            if k not in template:
+                continue
+            try:
+                datetime.strptime(template[k], "%Y-%m-%d")
+            except ValueError:
+                raise ValueError("Incorrect data format, should be yyyy-MM-dd")
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    from sys import exit
+
+    basicConfig(level=INFO, format="%(levelname)s %(asctime)s: %(message)s")
+
+    parser = ArgumentParser("Register affliated issues to Backlog project.")
+    parser.add_argument("path_to_template", help="Path to template file (toml).")
+    args = parser.parse_args()
+    template = toml.load(open(args.path_to_template))
+
+    # confirmation of variable replacement
+    if "repl" in template:
+        logger.info("--- Variables in template will be replaced as shown below. ---")
+        for k, v in template["repl"].items():
+            logger.info("{{{}}} = '{}'".format(k, v))
+        if input("Do you want to proceed? (Y/n): ").lower() != "y":
+            logger.info("--- Terminated by user input. ---")
+            exit()
+
+    bp = BacklogProject.by_config()
+    bp.post_affiliated_issues_by_template(template)
